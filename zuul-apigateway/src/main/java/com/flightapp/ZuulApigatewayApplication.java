ba894e83af340c87bdea9867d5221a6416a58ca7
Pratap Singh <pratapprem279@gmail.com>
2021-12-25 16:29:11 +0530

    Initial commit

---
+++ zuul-apigateway/src/main/java/com/flightapp/ZuulApigatewayApplication.java
@@ -0,0 +1,17 @@
+package com.flightapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ZuulApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApigatewayApplication.class, args);
+	}
+
+}
